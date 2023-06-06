b0c602b85f10a76f779b0b819c96ee7357910e82
kevincallaghan <kevincallaghan5@yahoo.com>
2023-06-06 18:40:47 -0500

    added the Apollo stuff to the serve, schema config

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,15 @@
+const { gql } = require('apollo-server-express');
+
+//! We need to add the user fields (inside type User below this comment) - I think we need to add an ID field to the model
+
+const typeDefs = gql`
+  type User {
+  
+  }
+
+  type Query {
+    users: [User]
+  }
+`;
+
+module.exports = typeDefs;
