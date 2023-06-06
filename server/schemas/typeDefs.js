8b81bb99fde1df6ca020139ebe8d643f56f9ee2e
kevincallaghan <kevincallaghan5@yahoo.com>
2023-06-06 18:50:47 -0500

    another typeDefs change

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,9 @@
 const { gql } = require('apollo-server-express');
 
-const { gql } = require('apollo-server-express');
-
 //! We need to add the user fields inside type User below this comment) - I think we need to add an ID field to the model
 
 const typeDefs = gql`
   type User {
-    _id: ID
     username: String
     email: String
     password: String
