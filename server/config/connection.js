a3e6a35ade5c5825c74efba115696129c4b1367f
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-06-07 14:48:41 -0500

    IAN FIXED IT

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 mongoose.connect(
   process.env.MONGODB_URI ||
     "mongodb+srv://MCReagan:michael312@classactivities.2d6rdyh.mongodb.net/taskmaster-flex",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
+  // {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   // useCreateIndex: true,
+  //   // useFindAndModify: false,
+  // }
 );
 
 module.exports = mongoose.connection;
