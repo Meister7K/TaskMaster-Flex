2d2d84ec0610c88bcd5948172d5be9459163b333
kevincallaghan <kevincallaghan5@yahoo.com>
2023-06-08 16:16:50 -0500

    removed Phaser to fix error

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,15 +8,3 @@ mongoose.connect(""+process.env.MONGODB_URI ,
 );
 
 module.exports = mongoose.connection;
-
-
-//TODO - Below is what we will use for Apollo
-
-// const mongoose = require('mongoose');
-
-// //! Insert your MongoDB URL in the single-quotes below
-// mongoose.connect(
-//   process.env.MONGODB_URI || ''
-// );
-
-// module.exports = mongoose.connection;
