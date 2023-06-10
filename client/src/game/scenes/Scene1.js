6293ff4d48e48a3df8907e8a4f8aa2ba9fa9a37d
7K <kmanfinkel17@gmail.com>
2023-06-10 18:13:07 -0500

    broken garbage

--- client/src/game/scenes/Scene1.js
+++ client/src/game/scenes/Scene1.js
@@ -4,19 +4,21 @@ import Pony from "../game-assets/pixel-pony.png";
 import Bat from "../game-assets/bat.png";
 
 
+
 class Scene1 extends Phaser.Scene {
   constructor() {
     super("loadGame");
   }
   preload() {
-    // this.load.image("pony", Pony);
+    //this.load.image("pony", 'game-assets/pixel-pony.png');
 
     // this.load.spritesheet("bat", Bat, {
     //   frameWidth: 36,
     //   frameHeight: 36,
     // });
 
-    this.load.image("background", Back);
+    //this.load.image("background", Back);
+
   }
 
   create() {
