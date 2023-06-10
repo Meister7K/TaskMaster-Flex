f63e0ee40abf27a27d368fd80804cbed6ea264ff
kevincallaghan <kevincallaghan5@yahoo.com>
2023-06-10 18:30:14 -0500

    adding pages

---
+++ client/src/gamePages/GameProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Inventory from '../components/inventory/Inventory'
+
+function GameProfile() {
+  return (
+    <>
+      <Inventory />
+    </>
+  )
+}
+
+export default GameProfile;
