7a35e5487ea32221cd6c483550ae51b8b8187b0d
Masterchef842 <robertwilliamlord55@gmail.com>
2023-06-10 11:32:35 -0500

    git links

--- client/src/components/about-cards/AboutCards.js
+++ client/src/components/about-cards/AboutCards.js
@@ -1,51 +1,61 @@
-import React from 'react'
-import CardItem from '../card-item/CardItem'
-import './AboutCards.css'
+import React from "react";
+import CardItem from "../card-item/CardItem";
+import "./AboutCards.css";
 
 function AboutCards() {
   return (
     <>
-     <h1 className='about'>About the Developers</h1>
-       <div className='cards'>
+      <h1 className="about">About the Developers</h1>
+      <div className="cards">
         <div className="cards-container">
           <div className="cards-wrapper">
             <ul className="cards-items">
-              <CardItem 
-              src="images/developer.jpg"
-              text="Will Lord"
-              label="Will Lord"
-              path="/Will"
-              />
-              <CardItem 
-              src="images/developer.jpg"
-              text="Marquise West"
-              label="Marquise West"
-              path="/Marquise"
-              />
-              <CardItem 
-              src="images/developer.jpg"
-              text="Kevin Callaghan"
-              label="Kevin Callaghan"
-              path="/Kevin"
-              />
-              <CardItem 
-              src="images/developer.jpg"
-              text="Karl Finkel"
-              label="Karl Finkel"
-              path="/Karl"
-              />
-              <CardItem 
-              src="images/developer.jpg"
-              text="Clint Reagan"
-              label="Clint Reagan"
-              path="/Clint"
-              />                        
-            </ul>          
+              
+                <CardItem
+                  src="images/developer.jpg"
+                  text="Will Lord"
+                  label="Will Lord"
+                  path="https://github.com/Masterchef842"
+                />
+          
+              
+                <CardItem
+                  src="images/developer.jpg"
+                  text="Marquise West"
+                  label="Marquise West"
+                  path="https://github.com/sudo-apt-install"
+                />
+          
+              
+                <CardItem
+                  src="images/developer.jpg"
+                  text="Kevin Callaghan"
+                  label="Kevin Callaghan"
+                  path="https://github.com/Meister7K"
+                />
+          
+              
+                <CardItem
+                  src="images/developer.jpg"
+                  text="Karl Finkel"
+                  label="Karl Finkel"
+                  path="https://github.com/kevincallaghan"
+                />
+          
+              
+                <CardItem
+                  src="images/developer.jpg"
+                  text="Clint Reagan"
+                  label="Clint Reagan"
+                  path="https://github.com/MCReagan"
+                />
+          
+            </ul>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default AboutCards
+export default AboutCards;
