b1ac7f1c956aa7a02fcd1323d31adb29a709cf8c
7K <kmanfinkel17@gmail.com>
2023-06-10 10:57:19 -0500

    fixed phaser

--- client/src/game/start.js
+++ client/src/game/start.js
@@ -1,14 +1,14 @@
-import Phaser from 'phaser';
-import Scene1 from './scenes/Scene1'
-import Scene2 from './scenes/Scene2'
+// import Phaser from 'phaser';
+// import Scene1 from './scenes/Scene1'
+// import Scene2 from './scenes/Scene2'
 
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: 'black',
-    scene:[Scene1, Scene2]
-};
+// const config = {
+//     type: Phaser.AUTO,
+//     width: 800,
+//     height: 600,
+//     backgroundColor: 'black',
+//     scene:[Scene1, Scene2]
+// };
 
-const game = new Phaser.Game(config);
+// const game = new Phaser.Game(config);
 
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { appendScript } from '../utils/appender'
-import start from '../game/start'
+//import { appendScript } from '../utils/appender'
+import Phaser from 'phaser';
+import Scene1 from '../../src/game/scenes/Scene1'
+import Scene2 from '../../src/game/scenes/Scene2'
 
 //TODO fix script for phazer 
 
 
 
-
-
 function Game() {
+
+  //TODO fix script for phazer 
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: 'black',
+  scene:[Scene1, Scene2] 
+};
+
+const game = new Phaser.Game(config);
 
   // appendScript('../../game/scenes/Scene1.js');
   // appendScript('../../game/scenes/Scene2.js')
@@ -16,7 +27,8 @@ function Game() {
 
   return (
     <>
-    <div className='canvas'>
+    <div className='game' game={game}>
+
     </div>
     </>
     
