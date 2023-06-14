c04ce13f3a9b08240503259974edc85539b1823c
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-06-14 00:19:17 -0500

    cant get text to resize correctly on rules list. Will revisit in the morning

--- client/src/components/rules/Rules.js
+++ client/src/components/rules/Rules.js
@@ -15,86 +15,89 @@ function Rules() {
 
   return (
     <>
-      <div id="rules"></div>
-      <div className="rules-container">
-        <div className="rules">
-          <h1>TASKMASTER FLEX:</h1>
-          <h2>RULES AND INFORMATION</h2>
-          <div className="rules-wrapper">
-            <ol className="rules-items">
-              <li>
-                Level Up Your Life: "Level up your life one task at a time."
-                Taskmaster Flex is here to transform your daily tasks into
-                exciting adventures!
-              </li>
-              <li>
-                Gamified Task Management: Taskmaster Flex turns your tasks into
-                a thrilling game. Earn experience points (XP), gold, and energy
-                for completing real-life tasks that you enter on the task page.
-              </li>
-              <li>
-                Customize Your Character: Create and customize your character to
-                embark on an epic journey. Level up your character and allocate
-                skill points to enhance your strength, health, damage, attack
-                speed, critical hit chance, or critical hit damage.
-              </li>
-              <li>
-                Shop for Power: Use the gold you earn to visit the in-game shop
-                and purchase items that boost your character's stats. Choose
-                wisely and strategize to become a formidable force in the game.
-              </li>
-              <li>
-                Battle Against Enemies: Face off against challenging enemies
-                that will attack your character. Be prepared to defend yourself
-                and use your skills wisely to emerge victorious.
-              </li>
-              <li>
-                Restore Health: As enemies attack, your character's health may
-                deplete. Restore your health by completing more real-life tasks
-                and regain the vitality to continue your adventure.
-              </li>
-              <li>
-                Exciting In-Game Actions: Spend energy to perform various
-                in-game actions such as opening chests, fighting boss battles,
-                and traveling to new areas. Conserve and manage your energy
-                wisely to explore more of the game world.
-              </li>
-              <li>
-                Level Progression: Gain experience points (XP) by completing
-                tasks and level up your character. Unlock new abilities,
-                rewards, and challenges as you progress through the game.
-              </li>
-              <li>
-                Stay Motivated: Taskmaster Flex provides a fun and engaging way
-                to stay motivated and accountable for your real-life tasks. Get
-                rewarded for your hard work and enjoy the satisfaction of
-                leveling up both in the game and in your life.
-              </li>
-              <li>
-                Embrace the Adventure: Taskmaster Flex is not just a task
-                management app; it's an immersive experience that transforms
-                your daily routine into a captivating adventure. Embark on a
-                journey of self-improvement and watch as your character and
-                productivity soar.
-              </li>
-            </ol>
-            Remember, Taskmaster Flex is here to help you level up your life one
-            task at a time. Are you ready to embark on this exciting quest and
-            become a true Taskmaster? Start your adventure today!
+      <div id="rules">
+        <div id="rules-bg">
+          <div className="rules-container">
+            <div className="rules">
+              <h2 className="rules-title">RULES AND INFORMATION:</h2>
+              <div className="rules-wrapper">
+                <ol className="rules-items">
+                  <li className="rules-list">
+                    Level Up Your Life: Transform your daily tasks into exciting
+                    adventures and level up your life one task at a time with
+                    Taskmaster Flex.
+                  </li>
+                  <li className="rules-list">
+                    Gamified Task Management: Turn your tasks into a thrilling
+                    game and earn experience points (XP), gold, and energy for
+                    completing real-life tasks on the task page.
+                  </li>
+                  <li className="rules-list">
+                    Customize Your Character: Create and customize your
+                    character to embark on an epic journey of task completion,
+                    leveling up, and enhancing your skills.
+                  </li>
+                  <li className="rules-list">
+                    Shop for Productivity: Use the gold you earn to visit the
+                    in-game shop and purchase items that boost your productivity
+                    and efficiency in real-life tasks.
+                  </li>
+                  <li className="rules-list">
+                    Battle Against Procrastination: Face off against the
+                    challenging enemy of procrastination, and use your skills
+                    wisely to stay focused, complete tasks, and emerge
+                    victorious.
+                  </li>
+                  <li className="rules-list">
+                    Restore Energy: Restore
+                    your energy by completing more real-life tasks and regain
+                    the vitality to continue your productive adventure.
+                  </li>
+                  <li className="rules-list">
+                    Exciting Task Actions: Spend your energy wisely to perform
+                    various in-game actions such as task prioritization,
+                    breaking tasks into smaller steps, and exploring new
+                    strategies for task completion.
+                  </li>
+                  <li className="rules-list">
+                    Level Progression: Gain experience points (XP) by completing
+                    tasks and level up your character, unlocking new abilities,
+                    rewards, and challenges as you progress in your real-life
+                    task management journey.
+                  </li>
+                  <li className="rules-list">
+                    Stay Motivated: Taskmaster Flex provides a fun and engaging
+                    way to stay motivated and accountable for your real-life
+                    tasks, getting rewarded for your hard work and enjoying the
+                    satisfaction of leveling up in both the game and your life.
+                  </li>
+                  <li className="rules-list">
+                    Embrace the Productivity Adventure: Taskmaster Flex is an
+                    immersive task management app that transforms your daily
+                    routine into a captivating adventure of self-improvement,
+                    boosting your character's productivity and watching your own
+                    productivity soar.
+                  </li>
+                </ol>
+                <div className="back-to-top">
+                  <Button
+                    className="btns"
+                    buttonstyle="bttbtn"
+                    onClick={scrollToTop}
+                    destination="#topOfPage"
+                    style={{
+                      fontFamily:
+                        "Augusta, cursive, Times New Roman, Times, serif",
+                    }}
+                  >
+                    <p className="bttbtn">Back to top?</p>
+                  </Button>
+                  <br />
+                  <br />
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="back-to-top">
-          <Button
-            className="btns"
-            buttonstyle="btn-outline-rules"
-            buttonSize="btn-large-rules"
-            onClick={scrollToTop}
-            destination="#topOfPage"
-          >
-            Back To Top
-          </Button>
-          <br />
-          <br />
         </div>
       </div>
     </>
