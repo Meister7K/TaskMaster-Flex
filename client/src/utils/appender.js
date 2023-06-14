85539cac1ae41b7785ed0c35d879d634fbe36d44
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-06-13 19:34:21 -0500

    Karl said I could do it. If it breaks, blame him

--- client/src/utils/appender.js
+++
@@ -1,7 +0,0 @@
-export const appendScript = (jsPath) =>{
-    const script = document.createElement('script');
-
-    script.src = jsPath;
-    script.async = true;
-    document.body.appendChild(script);
-}
