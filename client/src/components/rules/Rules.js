bf0ebf06708d813ff6f2253f1c3a51fe23c5b660
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-06-13 21:46:46 -0500

    added a custom font + more css

--- client/src/components/rules/Rules.js
+++ client/src/components/rules/Rules.js
@@ -23,31 +23,64 @@ function Rules() {
           <div className="rules-wrapper">
             <ol className="rules-items">
               <li>
-                This is where the rules will go. This is where the rules will
-                go. This is where the rules will go. This is where the rules
-                will go. This is where the rules will go.
+                Level Up Your Life: "Level up your life one task at a time."
+                Taskmaster Flex is here to transform your daily tasks into
+                exciting adventures!
               </li>
               <li>
-                This is where the rules will go. This is where the rules will
-                go. This is where the rules will go. This is where the rules
-                will go. This is where the rules will go.
+                Gamified Task Management: Taskmaster Flex turns your tasks into
+                a thrilling game. Earn experience points (XP), gold, and energy
+                for completing real-life tasks that you enter on the task page.
               </li>
               <li>
-                This is where the rules will go. This is where the rules will
-                go. This is where the rules will go. This is where the rules
-                will go. This is where the rules will go.
+                Customize Your Character: Create and customize your character to
+                embark on an epic journey. Level up your character and allocate
+                skill points to enhance your strength, health, damage, attack
+                speed, critical hit chance, or critical hit damage.
               </li>
               <li>
-                This is where the rules will go. This is where the rules will
-                go. This is where the rules will go. This is where the rules
-                will go. This is where the rules will go.
+                Shop for Power: Use the gold you earn to visit the in-game shop
+                and purchase items that boost your character's stats. Choose
+                wisely and strategize to become a formidable force in the game.
               </li>
               <li>
-                This is where the rules will go. This is where the rules will
-                go. This is where the rules will go. This is where the rules
-                will go. This is where the rules will go.
+                Battle Against Enemies: Face off against challenging enemies
+                that will attack your character. Be prepared to defend yourself
+                and use your skills wisely to emerge victorious.
+              </li>
+              <li>
+                Restore Health: As enemies attack, your character's health may
+                deplete. Restore your health by completing more real-life tasks
+                and regain the vitality to continue your adventure.
+              </li>
+              <li>
+                Exciting In-Game Actions: Spend energy to perform various
+                in-game actions such as opening chests, fighting boss battles,
+                and traveling to new areas. Conserve and manage your energy
+                wisely to explore more of the game world.
+              </li>
+              <li>
+                Level Progression: Gain experience points (XP) by completing
+                tasks and level up your character. Unlock new abilities,
+                rewards, and challenges as you progress through the game.
+              </li>
+              <li>
+                Stay Motivated: Taskmaster Flex provides a fun and engaging way
+                to stay motivated and accountable for your real-life tasks. Get
+                rewarded for your hard work and enjoy the satisfaction of
+                leveling up both in the game and in your life.
+              </li>
+              <li>
+                Embrace the Adventure: Taskmaster Flex is not just a task
+                management app; it's an immersive experience that transforms
+                your daily routine into a captivating adventure. Embark on a
+                journey of self-improvement and watch as your character and
+                productivity soar.
               </li>
             </ol>
+            Remember, Taskmaster Flex is here to help you level up your life one
+            task at a time. Are you ready to embark on this exciting quest and
+            become a true Taskmaster? Start your adventure today!
           </div>
         </div>
         <div className="back-to-top">
