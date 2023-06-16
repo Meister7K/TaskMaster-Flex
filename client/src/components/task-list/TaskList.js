765d8473ef49d5e9d55ad655996b20d4df5bfaa4
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-06-15 21:09:26 -0500

    audio shows on logged out

--- client/src/components/task-list/TaskList.js
+++ client/src/components/task-list/TaskList.js
@@ -91,151 +91,161 @@ function TaskList() {
             <div className="task-list-header">
               <h1>Task List</h1>
             </div>
-          <div className="task-form-wrapper">
-            <form onSubmit={handleSubmitTask}>
-              <div className="question">
-                <label>
-                  <div className="label-name">
-                    Task Name:
-                  </div>
-                  <input
-                    type="text"
-                    name="name"
-                    value={taskState.name}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="question">
-                <label>
-                  <div className="label-name">
-                    Difficulty:
-                  </div>
-                  <select
-                    name="difficulty"
-                    value={taskState.difficulty}
-                    onChange={handleChange}
-                  >
-                    <option value="none">Select difficulty</option>
-                    <option value="Easy">Easy</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Hard">Hard</option>
-                  </select>
-                </label>
-              </div>
-              <div className="question">
-                <label>
-                  <div className="label-name">
-                    Category:
-                  </div>
-                  <select
-                    name="category"
-                    value={taskState.category}
-                    onChange={handleChange}
-                  >
-                    <option value="none">Select category</option>
-                    <option value="School">School</option>
-                    <option value="Work">Work</option>
-                    <option value="Chores">Chores</option>
-                    <option value="Creative">Creative</option>
-                    <option value="Health">Health</option>
-                    <option value="Exercise">Exercise</option>
-                  </select>
-                </label>
-              </div>
-              <div className="question">
-              <button type="submit" className="addTaskButton">Add Task</button>
-              </div>
-            </form>
-          </div>
+            <div className="task-form-wrapper">
+              <form onSubmit={handleSubmitTask}>
+                <div className="question">
+                  <label>
+                    <div className="label-name">Task Name:</div>
+                    <input
+                      type="text"
+                      name="name"
+                      value={taskState.name}
+                      onChange={handleChange}
+                    />
+                  </label>
+                </div>
+                <div className="question">
+                  <label>
+                    <div className="label-name">Difficulty:</div>
+                    <select
+                      name="difficulty"
+                      value={taskState.difficulty}
+                      onChange={handleChange}
+                    >
+                      <option value="none">Select difficulty</option>
+                      <option value="Easy">Easy</option>
+                      <option value="Medium">Medium</option>
+                      <option value="Hard">Hard</option>
+                    </select>
+                  </label>
+                </div>
+                <div className="question">
+                  <label>
+                    <div className="label-name">Category:</div>
+                    <select
+                      name="category"
+                      value={taskState.category}
+                      onChange={handleChange}
+                    >
+                      <option value="none">Select category</option>
+                      <option value="School">School</option>
+                      <option value="Work">Work</option>
+                      <option value="Chores">Chores</option>
+                      <option value="Creative">Creative</option>
+                      <option value="Health">Health</option>
+                      <option value="Exercise">Exercise</option>
+                    </select>
+                  </label>
+                </div>
+                <div className="question">
+                  <button type="submit" className="addTaskButton">
+                    Add Task
+                  </button>
+                </div>
+              </form>
+            </div>
             <div className="active-tasks-header">
               <h2>Active Tasks:</h2>
             </div>
-          <div className="active-tasks-container">
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <ul className="task-list">
-                {activeTasks.map((task) => (
-                  <li key={task._id} className="task">
-                    <div className="task-card">
-                      <div className="task-details">
-                        <div className="task-name-container">
-                          <span className="task-name">{task.name}</span>
-                        </div>
-                        <div className="task-stuff-container">
-                          <div className="task-difficulty-wrapper">
-                            <span className="task-difficulty-label">
-                              Difficulty:
-                            </span>
-                            <span className={`task-difficulty ${task.difficulty.toLowerCase()}`}>
-                              {task.difficulty}
+            <div className="active-tasks-container">
+              {loading ? (
+                <div>Loading...</div>
+              ) : (
+                <ul className="task-list">
+                  {activeTasks.map((task) => (
+                    <li key={task._id} className="task">
+                      <div className="task-card">
+                        <div className="task-details">
+                          <div className="task-name-container">
+                            <span className="task-name">{task.name}</span>
+                          </div>
+                          <div className="task-stuff-container">
+                            <div className="task-difficulty-wrapper">
+                              <span className="task-difficulty-label">
+                                Difficulty:
+                              </span>
+                              <span
+                                className={`task-difficulty ${task.difficulty.toLowerCase()}`}
+                              >
+                                {task.difficulty}
+                              </span>
+                            </div>
+                            <div className="task-category-wrapper">
+                              <span className="task-category">
+                                Category: {task.category}
+                              </span>
+                            </div>
+                          </div>
+                          <div className="task-created-container">
+                            <span className="task-completed-date">
+                              Created{" "}
+                              {new Date(
+                                parseInt(task.createdAt)
+                              ).toLocaleString()}
                             </span>
                           </div>
-                          <div className="task-category-wrapper">
-                            <span className="task-category">
-                              Category: {task.category}
-                            </span>
-                          </div>
                         </div>
-                        <div className="task-created-container">
-                          <span className="task-completed-date">
-                            Created{" "}
-                            {new Date(parseInt(task.createdAt)).toLocaleString()}
-                          </span>
+                        <div className="task-actions">
+                          <button onClick={() => handleCompleteTask(task._id)}>
+                            Complete
+                          </button>
+                          <button onClick={() => handleDeleteTask(task._id)}>
+                            Delete
+                          </button>
                         </div>
                       </div>
-                      <div className="task-actions">
-                        <button onClick={() => handleCompleteTask(task._id)}>
-                          Complete
-                        </button>
-                        <button onClick={() => handleDeleteTask(task._id)}>
-                          Delete
-                        </button>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
             <div className="completed-tasks-header">
               <h2>Completed Tasks:</h2>
             </div>
-          <div className="completed-tasks-container">
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <ul className="task-list">
-                {completedTasks.map((task) => (
-                  <li key={task._id} className="task">
-                    <div className="task-card">
-                      <div className="task-details">
-                        <span className="task-name">{task.name}</span>
-                        <div className="task-stuff-container">
-                          <div className="task-difficulty-wrapper">
-                            <span className="task-difficulty">
-                              Difficulty: {task.difficulty}
+            <div className="completed-tasks-container">
+              {loading ? (
+                <div>Loading...</div>
+              ) : (
+                <ul className="task-list">
+                  {completedTasks.map((task) => (
+                    <li key={task._id} className="task">
+                      <div className="task-card">
+                        <div className="task-details">
+                          <span className="task-name">{task.name}</span>
+                          <div className="task-stuff-container">
+                            <div className="task-difficulty-wrapper">
+                              <span className="task-difficulty">
+                                Difficulty: {task.difficulty}
+                              </span>
+                            </div>
+                            <div className="task-category-wrapper">
+                              <span className="task-category">
+                                Category: {task.category}
+                              </span>
+                            </div>
+                          </div>
+                          <div className="task-created-container">
+                            <span className="task-completed-date">
+                              Created{" "}
+                              {new Date(
+                                parseInt(task.createdAt)
+                              ).toLocaleString()}
                             </span>
                           </div>
-                          <div className="task-category-wrapper">
-                            <span className="task-category">
-                              Category: {task.category}
+                          <div className="task-created-container">
+                            <span className="task-completed-date">
+                              Completed{" "}
+                              {new Date(
+                                parseInt(task.updatedAt)
+                              ).toLocaleString()}
                             </span>
                           </div>
-                        </div>
-                        <div className="task-created-container">
-                          <span className="task-completed-date">
-                            Completed{" "}
-                            {new Date(parseInt(task.updatedAt)).toLocaleString()}
-                          </span>
                         </div>
                       </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            )}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
           </div>
         </div>
