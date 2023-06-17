aa7cd305c050e9a887efa38a3fbb14738817a10c
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-06-17 18:39:22 -0500

    changed JWT expiration

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const secret = process.env.SECRET;
-const expiration = "2h";
+const expiration = "8h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
