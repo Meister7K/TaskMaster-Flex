9e9a9603e372ffae30b629fbf59ec58e7e175e5d
kevincallaghan <kevincallaghan5@yahoo.com>
2023-06-19 14:31:29 -0500

    working on minotaur animation for new class

--- client/src/game/scenes/Scene1.js
+++ client/src/game/scenes/Scene1.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import Back from "../game-assets/map-files/Taskmap1.png";
 import Minotaur from '../game-assets/gameSprites/MinotaurSpritesheet.png';
 import Warrior1 from "../game-assets/gameSprites/Warrior1SpriteNEW.png";
+import MinotaurSprites from "../game-assets/gameSprites/MinotaurATLAS.png";
 // import Warrior0Back from '../game-assets/gameSprites/Warrior0BackSpritesheet.png';
 // import Warrior0Left from '../game-assets/gameSprites/Warrior0LeftSpritesheet.png';
 // import Warrior0Right from '../game-assets/gameSprites/Warrior0RightSpritesheet.png';
@@ -12,6 +13,7 @@ import Warrior1 from "../game-assets/gameSprites/Warrior1SpriteNEW.png";
 import startMap from '../game-assets/map-files/Map.json'
 import tiles from'../game-assets/map-files/tileSet1.png'
 import Warrior1Atlas from '../game-assets/gameSprites/Warrior1ATLAS.json'
+import MinotaurAtlas from '../game-assets/gameSprites/MinotaurATLAS.json'
 
 
 class Scene1 extends Phaser.Scene {
@@ -28,16 +30,25 @@ class Scene1 extends Phaser.Scene {
  this.load.image('tile1', tiles);
     this.load.tilemapTiledJSON('map1',startMap);
 
-    this.load.spritesheet("minotaur", Minotaur, {
-      frameWidth: 96,
-      frameHeight: 80,
-    });
+    //! Hidden to test atlas
+    // this.load.spritesheet("minotaur", Minotaur, {
+    //   frameWidth: 96,
+    //   frameHeight: 80,
+    // });
 
     // this.load.spritesheet(Warrior1);
     this.load.atlas(
       "a-warrior1",
       Warrior1,
       Warrior1Atlas,
+      null,
+      Phaser.Loader.TEXTURE_ATLAS_JSON_HASH
+    );
+
+    this.load.atlas(
+      "minotaur",
+      MinotaurSprites,
+      MinotaurAtlas,
       null,
       Phaser.Loader.TEXTURE_ATLAS_JSON_HASH
     );
